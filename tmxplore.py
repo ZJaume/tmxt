9a89a6cb519e4895bfe56b47bc468f941d878d17
ZJaume <jzaragoza@prompsit.com>
2022-02-18 18:36:13 +0100

    Explore propierties and add --delimiter option

--- tmxplore.py
+++ tmxplore.py
@@ -6,6 +6,9 @@ Usage:
 Options:
   --no_tus=<n>  Explore the first n translation units at much [default: 10].
   --all         Explore all the file.
+  --props       Explore props type.
+  --delimiter=<d>
+                Delimiter to the output codelist [default:  ]
   
 I/O Defaults:
   INPUT_FILE        Defaults to stdin.
@@ -15,15 +18,16 @@ from docopt import docopt
 import xml.parsers.expat
 import sys
 
-def print_result(langlist):
-    print(" ".join(langlist))
+def print_result(langlist, delimiter=' '):
+    print(delimiter.join(langlist))
     sys.exit(0)
 
-def explore(fd, ntus=10):
+def explore(fd, ntus=10, props=False):
     langset  = set()
     langlist = []
+    proplist = []
     ntu = 0
-    
+
     def se(name, attrs):
         nonlocal ntu
         if name == "tuv":
@@ -32,6 +36,9 @@ def explore(fd, ntus=10):
             elif "lang" in attrs:
                 lang = attrs["lang"].lower()
 
+            if props:
+                lang = 'seg_' + lang
+
             if lang not in langset:
                 langlist.append(lang)
                 langset.add(lang)
@@ -39,22 +46,27 @@ def explore(fd, ntus=10):
         elif name == "tu":
             ntu += 1
             if ntu >= ntus + 1:
-                print_result(langlist)
-    
+                return langlist + proplist
+
+        elif props and name == "prop":
+            if attrs["type"] not in proplist:
+                proplist.append(attrs["type"])
+
     p = xml.parsers.expat.ParserCreate()
     p.StartElementHandler = se
     p.ParseFile(fd)
 
-    print_result(langlist)
+    return langlist + proplist
 
 def main():
     arguments = docopt(__doc__, version='tmxplore 1.0')
 
     fd = sys.stdin.buffer if not arguments["INPUT_FILE"] else open(arguments["INPUT_FILE"], "rb")
     if arguments["--all"]:
-        explore(fd, sys.maxsize)
+        codelist = explore(fd, sys.maxsize, arguments["--props"])
     else:
-        explore(fd, int(arguments["--no_tus"]))        
+        codelist = explore(fd, int(arguments["--no_tus"]), arguments["--props"])
+    print_result(codelist, arguments["--delimiter"])
 
     fd.close()
 
