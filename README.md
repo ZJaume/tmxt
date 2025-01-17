e99a4a857a636a290080b537f09d9cf849361f92
ZJaume <jzaragoza@prompsit.com>
2025-01-17 16:45:15 +0100

    Multiprops support

--- README.md
+++ README.md
@@ -41,3 +41,15 @@ Printing TU propierties
 $ python3 tmxt.py --codelist en,fr,prop1,prop2 tm.fr-en.tmx en-fr.txt
 ```
 
+For propierties inside the TUV, the language code must be prepended
+```bash
+$ python3 tmxt.py --codelist en,fr,en-prop1,fr-prop1,prop2 tm.fr-en.tmx en-fr.txt
+```
+
+In case a propierty is repeated multiple times inside the same TU or TUV, the last instance will be printed.
+To get all of them, add `--multiprops` option and all the propierty instances will be printed in the same TSV field but encoded as a JSON array.
+The property can be easily parsed in pythin like
+```python
+import json
+json.loads(fields[3])
+```
--- tmxt.py
+++ tmxt.py
@@ -2,7 +2,7 @@
 specified.
 
 Usage:
-  tmxt.py --codelist=<langcodes> [--header] [--keep_seg] [--strict] [INPUT_FILE [OUTPUT_FILE]]
+  tmxt.py --codelist=<langcodes> [--header] [--keep_seg] [--strict] [--multiprops] [INPUT_FILE [OUTPUT_FILE]]
 
 Options:
   --codelist=<langcodes>   Comma-separated list of langcodes (i.e. "en,es").
@@ -10,6 +10,8 @@ Options:
   --header                 Print header with column names from --codelist
   --keep_seg               Don't remove 'seg_' prefix from header fields
   --strict                 Don't ignore SIGPIPE
+  --multiprops             If there are multiple props with tha same name in a TU or TUV, print JSON array
+                           When not enabled, will print the last one only
   
 I/O Defaults:
   INPUT_FILE               Defaults to stdin.
@@ -19,9 +21,10 @@ I/O Defaults:
 from docopt import docopt
 import re
 import sys
+import json
 import xml.parsers.expat
 
-def process_tmx(input, output, codelist):
+def process_tmx(input, output, codelist, multiprops=False):
     curlang  = ""
     curtuv   = []
     intuv    = False
@@ -57,12 +60,29 @@ def process_tmx(input, output, codelist):
         elif name == "prop":
             intprop = True
             curtype = attrs['type']
+            if curlang:
+                # prepend the lang of the tuv to differentiate between tuv props with the same name
+                curtype = f"{curlang}-{curtype}"
             curprop = []
             
     def ee(name):
-        nonlocal intuv, intprop, curtuv, curprop, curtype, p1, p2, tu, curlang, codelist, numlangs, fmt, output, inph
+        nonlocal intuv, intprop, curtuv, curprop, curtype, p1, p2, tu, curlang, codelist, numlangs, fmt, output, inph, multiprops
         if name == "tu":
-            output.write(fmt.format(*[tu[code] if not isinstance(tu[code], list) else '\t'.join(tu[code][:numlangs]) for code in codelist]))
+            # print all collected fields
+            # the props are printed as a json array
+            tsv_fields = []
+            for code in codelist:
+                if not isinstance(tu[code], list):
+                    tsv_fields.append(tu[code])
+                elif multiprops:
+                    tsv_fields.append(json.dumps(tu[code]))
+                else:
+                    if tu[code]:
+                        tsv_fields.append(tu[code][0])
+                    else:
+                        tsv_fields.append("")
+
+            output.write(fmt.format(*tsv_fields))
             numlangs = 0
 
         elif name == "seg":
@@ -106,6 +126,7 @@ def main():
     header = arguments['--header']
     keep_seg = arguments['--keep_seg']
     strict_mode = arguments['--strict']
+    multiprops = arguments['--multiprops']
     
     try:
         if len(codelist) > 0:
@@ -115,7 +136,7 @@ def main():
                     output.write('\t'.join(codelist_seg) + '\n')
                 else:
                     output.write('\t'.join(codelist) + '\n')
-            process_tmx(input, output, codelist)
+            process_tmx(input, output, codelist, multiprops)
 
         input.close()
         output.close()
